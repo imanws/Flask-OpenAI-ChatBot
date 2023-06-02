e9047de23e96ac34daaabb9b548fcd51e279a93a
iman <me@iman.ws>
2023-06-02 17:28:03 +0330

    chatbot v1.0

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+from gtts import gTTS
+app = Flask(__name__)
+
+#OpenAI API KEY
+openai.api_key = '.....'
+
+#HOME Page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    message = request.form['message']
+
+    response  = openai.ChatCompletion.create(
+        messages=[{"role": "user", "content":message}],
+        model = "gpt-3.5-turbo",
+        temperature = 0.7,max_tokens = 4000)
+   
+    reply = response.choices[0].message["content"].encode('utf-8')
+
+    return (reply.decode('utf-8'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
